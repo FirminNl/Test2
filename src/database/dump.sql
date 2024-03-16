a005526fa5fe7032ff03ccf09b8097ab88841ce2
FirminNl <93434503+FirminNl@users.noreply.github.com>
2024-03-16 22:01:14 +0100

    first commit

--- src/database/dump.sql
+++ src/database/dump.sql
@@ -1,3 +1,36 @@
+CREATE SCHEMA IF NOT EXISTS `datingapp` DEFAULT CHARACTER SET utf8 ;
+USE `datingapp`;
+
+DROP TABLE IF EXISTS `userprofile`;
+CREATE TABLE `userprofile` (
+`id` int NOT NULL DEFAULT '0',
+`timestamp` timestamp,
+`google_user_id` varchar(100) NOT NULL DEFAULT '',
+`email` varchar(100) NOT NULL DEFAULT '',
+`firstname`varchar(100) NOT NULL DEFAULT'',
+`surname`varchar(100) NOT NULL DEFAULT'',
+`about_me` varchar(500) NOT NULL DEFAULT '',
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `searchprofile`;
+CREATE TABLE `searchprofile` (
+`id` int NOT NULL DEFAULT '0',
+`timestamp` timestamp,
+`userprofile_id` int NOT NULL DEFAULT '0',
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `matching`;
+CREATE TABLE `matching` (
+`id` int NOT NULL DEFAULT '0',
+`timestamp` timestamp,
+`userprofile_id` int NOT NULL DEFAULT '0',
+`candidateprofile_id` int NOT NULL DEFAULT '0',
+`unseen_profile` boolean,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
 DROP TABLE IF EXISTS `similarity`;
 CREATE TABLE `similarity` (
 `id` int NOT NULL DEFAULT '0',
